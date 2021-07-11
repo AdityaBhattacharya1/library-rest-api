c74ee3d0ab26562421d864445a824d9d57df12c5
AdityaBhattacharya1 <69953590+AdityaBhattacharya1@users.noreply.github.com>
2021-07-11 19:29:37 +0530

    remove the temp fix and implement proper process termination

--- __test__/auth.test.js
+++ __test__/auth.test.js
@@ -7,47 +7,60 @@ const User = require('../src/models/User')
 
 const { testDBSetup } = require('../src/utils/testDBSetup')
 
-process.env.NODE_ENV = 'test'
+testDBSetup()
 
 describe('Auth tests', () => {
 	beforeAll(() => {
 		Books.deleteMany({})
-		testDBSetup()
-		console.log('Set up db for tests')
 	})
+
+	// beforeEach(() => {
+	// 	User.deleteMany({ email: { $eq: 'testing@testtest.com' } })
+	// })
+
 	afterEach(() => {
 		User.deleteMany({ email: { $eq: 'testing@testtest.com' } })
 	})
+
 	afterAll(async () => {
 		await mongoose.connection.close()
 	})
+
 	it('should return a 400 response', async () => {
 		const res = await request.post('/api/user/login')
+
 		expect(res.status).toBe(400)
 	})
+
 	it('should return a 401 response', async () => {
 		const testUser = {
 			email: 'testing@testtest.com',
 			password: 'totallysecurepasswordnohaxpls',
 		}
+
 		const res = await request.post('/api/user/login').send(testUser)
 
 		expect(res.status).toBe(200)
 	})
+
 	it('should return a 400 response', async () => {
 		const res = await request.post('/api/user/register')
+
 		expect(res.status).toBe(400)
 	})
+
 	it('should return a 401 response', async () => {
 		const res = await request.post('/api/books/new')
 
 		expect(res.status).toBe(401)
 	})
+
 	it('should return a 401 response', async () => {
 		const res = await request.patch('/api/books/update/nonexistentbook')
 
 		expect(res.status).toBe(401)
 	})
+
 	it('should return a 401 response', async () => {
 		const res = await request.delete('/api/books/delete/nonexistentbook')
 
--- __test__/general.test.js
+++ __test__/general.test.js
@@ -2,32 +2,41 @@ const app = require('../app')
 const supertest = require('supertest')
 const request = supertest(app)
 const Books = require('../src/models/Books')
-process.env.NODE_ENV = 'test'
+const mongoose = require('mongoose')
 
 describe('test', () => {
 	beforeEach(() => {
 		Books.deleteMany({})
 	})
+
+	afterAll(() => {
+		mongoose.connection.close()
+	})
+
 	it('Should get 0 books from the books endpoint', async () => {
 		const res = await request.get('/api/books/')
+
 		expect(res.status).toBe(200)
 		expect(res.body).toEqual([])
 	})
 
 	it('Should get 0 books by id from the /get endpoint', async () => {
 		const res = await request.get('/api/books/get/nonexistentbook')
+
 		expect(res.status).toBe(200)
 		expect(res.body).toEqual([])
 	})
 
 	it('Should get 0 books by author from the /author endpoint', async () => {
 		const res = await request.get('/api/books/author/nonexistentauthor')
+
 		expect(res.status).toBe(200)
 		expect(res.body).toEqual([])
 	})
 
 	it('Should get 0 books from the random book endpoint', async () => {
 		const res = await request.get('/api/books/random')
+
 		expect(res.status).toBe(200)
 		expect(res.body).toEqual([])
 	})
